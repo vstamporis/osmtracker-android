fe4e3525199df615e080b27d6ac8c842622d6a80
Vassilis <bzafiris@gmail.com>
2020-01-10 19:51:48 +0200

    Changed test

--- app/src/androidTest/java/net/osmtracker/BaristaTest.java
+++ app/src/androidTest/java/net/osmtracker/BaristaTest.java
@@ -2,12 +2,14 @@ package net.osmtracker;
 
 import android.Manifest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import net.osmtracker.activity.TrackManager;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import gr.aueb.android.barista.core.annotations.GeoFix;
 import gr.aueb.android.barista.core.annotations.Permission;
@@ -20,17 +22,19 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
+@RunWith(AndroidJUnit4.class)
 public class BaristaTest {
 
     @Rule
     public ActivityTestRule<TrackManager> activityActivityTestRule = new ActivityTestRule<TrackManager>(TrackManager.class);
 
     @Test
-    @GeoFix(lat = 37.975391, longt = 23.735524)
-    //@Permission(Manifest.permission.ACCESS_FINE_LOCATION)
+    @GeoFix(lat = 38.2, longt = 23.4)
+    @Permission(Manifest.permission.ACCESS_FINE_LOCATION)
     public void testBarista() {
         onView(withText("Track list:")).check(matches(isDisplayed()));
+        waitFor(3);
+
     }
 
     @Before
@@ -38,5 +42,13 @@ public class BaristaTest {
         activityActivityTestRule.getActivity().getContentScene();
     }
 
+
+    protected void waitFor(int seconds){
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
